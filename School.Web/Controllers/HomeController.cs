f93aac7aba7df0ae064c432fb28626d3a6d26df4
temain <asv.artm@gmail.com>
2018-04-20 01:28:26 +0300

    Добавьте файлы проекта.

---
+++ School.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School.Web.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+            return View();
+        }
+
+        public ActionResult Students()
+        {
+            return View();
+        }
+
+        public ActionResult Disciplines()
+        {
+            return View();
+        }
+
+        public ActionResult Journal()
+        {
+            return View();
+        }
+    }
+}
